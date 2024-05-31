e655deca11cd9881fb51af9cc3e9cb7b80e2b591
Ahmed Yusuf <ahmedyousif1322@gmail.com>
2024-05-31 15:10:18 +0300

    working time until break

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,81 @@
 'use client'
 // import Image from "next/image";
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 import 'react-clock/dist/Clock.css';
+// import 'clock.css';
+
 import { useEffect, useState } from 'react';
 import Clock from 'react-clock';
+
+
+
 export default function Home() {
   const [value, setValue] = useState(new Date());
+  const [timeUntilBreak, setTimeUntilBreak] = useState('wait...');
+// const [min, setMin] = useState(value.getMinutes());
+const [sec, setSec] = useState(2);
+
+
+function everySecond() {
+  setSec(value.getSeconds())
+
+  if (min < 25) {
+    setTimeUntilBreak(`time until break: ${25 - min} min`);
+  } else if (min >= 25 && min <= 30) {
+    console.log(sec);
+
+    setTimeUntilBreak(`Break: ${30 - min} min`);
+  } else if (min < 50) {
+    setTimeUntilBreak(`time until break: ${50 - min} min`);
+  } else if (min >= 50 && min <= 60) {
+    setTimeUntilBreak(`Break: ${60 - min} min`);
+  }
+  setValue(new Date());
+}
 
   useEffect(() => {
-    const interval = setInterval(() => setValue(new Date()), 1000);
+    
+    const interval = setInterval(() => {
+      // setSec(  new Date().getSeconds())
+      console.log('hello');
+
+
+      let min = new Date().getMinutes();
+      if (min < 25) {
+        setTimeUntilBreak(`time until break: ${25 - min} min`);
+      } else if (min >= 25 && min <= 30) {
+        console.log(sec);
+    
+        setTimeUntilBreak(`Break: ${30 - min} min`);
+      } else if (min < 50) {
+        setTimeUntilBreak(`time until break: ${50 - min} min`);
+      } else if (min >= 50 && min <= 60) {
+        setTimeUntilBreak(`Break: ${60 - min} min`);
+      }
+
+
+      setValue(new Date());
+    }, 1000);
+
 
     return () => {
       clearInterval(interval);
     };
   }, []);
+
+  
+
+
   return (
-    <main >
+    <main className={styles.container}>
        {/* <p>Current time:</p> */}
-      <Clock value={value} renderNumbers={true} />
+       <div>
+       <Clock value={value} renderNumbers={true} /><br></br>
+      
+     <span className={styles.timeUntilText}>{timeUntilBreak }</span>
+       </div>
+
+     {/* <h1>{sec}</h1> */}
     </main>
   );
 }
